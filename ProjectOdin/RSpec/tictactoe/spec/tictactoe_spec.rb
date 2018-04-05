2cd508a31fd57845d548a3fcd37629b63e839e9b
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-04-05 07:46:56 -0400

    adding some tictactoe tests

--- ProjectOdin/RSpec/tictactoe/spec/tictactoe_spec.rb
+++ ProjectOdin/RSpec/tictactoe/spec/tictactoe_spec.rb
@@ -5,6 +5,7 @@ describe Players do
 
     it "Assigns the player name and symbol attributes to a Player object" do
       playerX = Players.new("Momo", "X")
+        expect(playerX).to be_instance_of(Players)
         expect(playerX.name).to eql("Momo")
         expect(playerX.symbol).to eql("X")
     end
@@ -14,69 +15,43 @@ end
 describe Board do
   context "When initializing the Board class" do
 
-    
+    it "Sets the board attribute set to a string representing the empty board" do
+      game = Board.new
+        expect(game.board).to eql("\n     --------------\n      a1 | a2 | a3\n     --------------\n      b1 | b2 | b3\n     --------------\n      c1 | c2 | c3\n     --------------")
+    end
   end
 end
-
-
-
 
 =begin
-describe "caesar_cipher" do
+describe "start" do
 
-  context "given an empty string" do
-    it "returns an empty string" do
-      expect(caesar_cipher("", 2)).to eql("")
+  context "given start method is called on an Board class object" do
+
+      it "Opens game by asking for player name" do
+        game = Board.new()
+        expect(game.start()).to eql("Do you want to play? Player 1, what's your name?")
+      end
+  end
+end
+=end
+
+describe Board, '#win_checker(marker)' do
+
+  context "given a marker and a completely empty board" do
+    it "checks the board for 3 symbols in consecutive spaces" do
+      board = Board.new
+      expect(board.win_checker("X")).to eql("play")
     end
   end
 
-  context "given 0 as the # of characters to shift by" do
-    it "returns the given string" do
-      expect(caesar_cipher("testmycipher", 0)).to eql("testmycipher")
-    end
-  end
-
-  context "given a string with spaces and letters" do
-    it "returns the spaces at the same index but the letters shifted" do
-      expect(caesar_cipher("test my cipher", 5)).to eql("yjxy rd hnumjw")
-    end
-  end
-
-  context "given a string with a mix of capitalized and lowercase letters" do
-    it "returns the letters shifted while retaining capitalization at the same indexes" do
-      expect(caesar_cipher("tEsTmycIPHer", 5)).to eql("yJxYrdhNUMjw")
-    end
-  end
-
-  context "given a string with the full range of the alphabet, looping on itself" do
-    it "returns the full range of the alphabet shifted by the given number" do
-      expect(caesar_cipher("abcdefghijklmnopqrstuvwxyz", 3)).to eql("defghijklmnopqrstuvwxyzabc")
-    end
-  end
-
-  context "given a string of only special characters and a shift number" do
-    it "returns the special characters unchanged" do
-      expect(caesar_cipher("!?*- %.//", 3)).to eql("!?*- %.//")
-    end
-  end
-
-  context "given a string with a mix of space, capitalized letters, and special characters" do
-    it "returns the shifted string with correct capitalization and special characters" do
-      expect(caesar_cipher("Test! My! Cipher!", 1)).to eql("Uftu! Nz! Djqifs!")
-    end
-  end
-
-  context "given a string and a negative value shift number" do
-    it "returns the letters shifted to the left of the alphabet instead of the right" do
-      expect(caesar_cipher("Test! My! Cipher!", -2)).to eql("Rcqr! Kw! Agnfcp!")
-    end
-  end
-
-  context "given a shift number that is greater than the length of the alphabet" do
-    it "returns the letters shifted by looping through the alphabet" do
-      expect(caesar_cipher("testmycipher", 27)).to eql("uftunzdjqifs")
+  context "given a marker and a winning board" do
+    it "checks the board for 3 symbols in consecutive spaces" do
+      board = Board.new
+        board.grid[:a1] = 'X'
+        board.grid[:b2] = 'X'
+        board.grid[:c3] = 'X'
+      expect(board.win_checker("X")).to eql("win")
     end
   end
 
 end
-=end
